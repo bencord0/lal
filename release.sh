d46694cd923be85cb183d22c9245f5cd66e2d63d
Ben Cordero <bencord0@condi.me>
2021-06-16 21:31:00 +0100

    Update release script

--- release.sh
+++ release.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 set -ex
-
-ver=$(grep version Cargo.toml | head -n 1 | awk -F'"' '{print $2}')
-
-git tag -a "v${ver}" -m "${ver}"
+version="$(./version.awk < Cargo.toml)"
+git tag -a "v${versioon}" -m "${version}"
